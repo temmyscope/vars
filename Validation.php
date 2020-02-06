e134bb0f6da229867cc352ddd920520bf0baae15
temmyscope <esdentp@gmail.com>
2020-02-06 22:51:56 +0100

    Updated code for missing methods

--- Validation.php
+++ Validation.php
@@ -2,7 +2,7 @@
 namespace Seven\Vars;
 
 /**
- * Validate
+ * Validation
  *
  * @package Validator
  * @author 
@@ -15,7 +15,6 @@ class Validation
 	 * @var bool $_passed
 	 * @var Array $_errors
 	*/
-
 
 	private $source;
 	private $_passed = true;
@@ -42,7 +41,7 @@ class Validation
 	{
 		foreach($items as $item => $rules) {
 			$display = $rules['display'];
-			array_shift();
+			array_shift($rules);
 			foreach ($rules as $rule => $rule_value) {
 				$value = $this->source[$item];
 				if ($rule === 'required' && empty($value)) {
@@ -51,97 +50,75 @@ class Validation
 					switch((string) $rule){
 						case 'not_less_than':
 							if ( (int)$value < $rule_value) {
-								$this->_errors[] = ;
-								$this->status('error', ["{$display} can not be less than {$rule_value} characters.", $item]);
+								$this->_errors[] = "{$display} can not be less than {$rule_value} characters.";
 							}
 							break;
 						case 'not_greater_than':
 							if ( (int)$value > $rule_value) {
-								$this->status('error', ["{$display} can not be greater than {$rule_value} characters.", $item]);
+								$this->_errors[] = "{$display} can not be greater than {$rule_value} characters.";
 							}
 							break;
 						case 'min':
 							if ( mb_strlen($value) < $rule_value) {
-								$this->status('error', ["{$display} must be a minimum of {$rule_value} characters.", $item]);
+								$this->_errors[] = "{$display} must be a minimum of {$rule_value} characters.";
 							}
 							break;
 						case 'max':
 							if ( mb_strlen($value) > $rule_value) {
-								$this->status('error', ["{$display} must be a maximum of {$rule_value} characters.", $item]);
+								$this->_errors[] = "{$display} must be a maximum of {$rule_value} characters.";
 							}
 							break;
 						case 'len':
 							if ( mb_strlen($value) !== $rule_value) {
-								$this->status('error', ["{$display} must be exactly {$rule_value} characters.", $item]);
+								$this->_errors[] = "{$display} must be exactly {$rule_value} characters.";
 							}
 							break;
 						case 'matches':
 							if ($value != $source[$rule_value]){
 								$matchDisplay = $items[$rule_value]['display'];
-								$this->status('error', ["{$matchDisplay} and {$display} must match.", $item]);
-							}
-							break;
-						case 'matches':
-							if ($value != $source[$rule_value]){
-								$matchDisplay = $items[$rule_value]['display'];
-								$this->status('error', ["{$matchDisplay} and {$display} must match.", $item]);
-							}
-							break;
-						case 'unique':
-							if (!empty($table)){
-								$check = $this->_db->findBy($table, [$item => $value]);
-								if (!empty($check)) {
-									$this->status('error', ["{$display} already exists. Please choose another {$display}", $item]);
-								}
-							}else{
-								die("\$table is empty. The 'Unique' Validator requires a table to check.");
+								$this->_errors[] = "{$matchDisplay} and {$display} must match.";
 							}
 							break;
 						case 'is_numeric':
 							if (!is_numeric($value)) {
-								$this->status('error', ["{$display} has to be a number. Please use a numeric value.", $item]);
+								$this->_errors[] = "{$display} has to be a number. Please use a numeric value.";
 							}
 							break;
 						case 'valid_email':
 							if(!filter_var($value, FILTER_VALIDATE_EMAIL)){
-								$this->status('error', ["{$display} must be a valid email address.", $item]);
+								$this->_errors[] = "{$display} must be a valid email address.";
 							}
 							break;
 						case 'alpha':
 							if (!ctype_alpha($value)){
-								$this->status('error', ["{$display} can only be alphabeths", $item]);
+								$this->_errors[] = "{$display} can only be alphabeths";
 							}
 							break;
 						case 'alpha_num':
 							if (!ctype_alnum($value)){
-								$this->status('error', ["{$display} can only be alphabeths and or numbers.", $item]);
+								$this->_errors[] = "{$display} can only be alphabeths and or numbers.";
 							}
 							break;
 						case 'is_one_of': 
 							if(is_array($rule_value) && (!in_array($value, $rule_value)) ){
-								$this->status('error', ["{$display} can only be one of the given options", $item]);
+								$this->_errors[] = "{$display} can only be one of the given options";
 							}
 							break;
 						case 'equals':
 							if( $value !== $rule_value['value'] ){
-								$this->status('error', ["{$display} must be the same value as {$rule_value['display']}", $item]);
+								$this->_errors[] = "{$display} must be the same value as {$rule_value['display']}";
 							}
 							break;
 						case 'is_not':
 						case 'is_not_one_of':
 						case 'can_not_be':
 							if ( is_array($rule_value) && in_array($value, $rule_value) ){
-								$this->status('error', ["{$display} can not be any of :".implode(', ', $rule_value), $item]);
+								$this->_errors[] = "{$display} can not be any of :".implode(', ', $rule_value);
 							}
 							break;
 						case 'is_same_as':
 							if( $value !== $source[$rule_value] ){
-								$this->status('error', ["{$display} must be the same value as {$rule_value}", $item]);
-							}
-							break;
-						case 'is_file':
-							if (!is_file($value)($value)){
-								$this->status('error', ["{$display} must be a valid file type", $item]);
+								$this->_errors[] = "{$display} must be the same value as {$rule_value}";
 							}
 							break;
 					}
